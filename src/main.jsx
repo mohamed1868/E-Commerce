76525144d59236f3b5d8d07d74f2ffb67462670c
mohamed1868 <mohamedsayed20500@gmail.com>
2025-03-03 17:21:51 +0200

    test2

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistsStore} >
-    <BrowserRouter>
+    <BrowserRouter  basename="/E-Commerce">
       <App />
     </BrowserRouter>  
     </PersistGate>
